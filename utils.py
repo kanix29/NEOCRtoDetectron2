a84402562f5c7c416af86d75f3bf01cd284c3a6a
kanix29 <48993719+kanix29@users.noreply.github.com>
2020-08-30 18:30:32 +0900

    edit utils to use xmltodict module

--- utils.py
+++ utils.py
@@ -1,63 +1,55 @@
-import os
-import glob
-from tqdm import tqdm
-import cv2
 import numpy as np
-
-from xml.etree import ElementTree
-from xml.etree.ElementTree import Element, SubElement
-from lxml import etree
+import glob
+import xmltodict
+from tqdm import tqdm
+from tqdm.notebook import tqdm as tqdm_notebook
 
 from detectron2.structures import BoxMode
 
-
-def get_neocr_dicts(img_dir, xml_dir):
-    ENCODE_METHOD = 'utf-8'
-    num_files = len(os.listdir(xml_dir))
+def get_neocr_dicts(xml_dir):
     xml_files = glob.glob(f'{xml_dir}/*.xml')
     xml_files.sort()
 
     dataset_dicts = []
-    for idx, xml_file in tqdm(enumerate(xml_files), total=num_files):
+    for idx, xml_file in enumerate(tqdm_notebook(xml_files)):
+        # Load XML format to Dict
+        doc = xmltodict.parse(open(xml_file).read())
+
         record = {}
-        
-        # process XML
-        parser = etree.XMLParser(encoding=ENCODE_METHOD)
-        xmltree = ElementTree.parse(xml_file, parser=parser).getroot()
-
-        filename = os.path.join(img_dir, xmltree.find('filename').text)# ~.jpg
-        height, width = cv2.imread(filename).shape[:2]
-        
-        record["file_name"] = filename
+        record["file_name"] = doc['annotation']['filename']
         record["image_id"] = idx
-        record["height"] = height
-        record["width"] = width
-        
+        record["height"] = doc['annotation']['properties']['height']
+        record["width"] = doc['annotation']['properties']['width']
+
+        # for single object
+        if not type(doc['annotation']['object']) == list:
+            doc['annotation']['object'] = [doc['annotation']['object']]
+
         objs = []
-        x, y = 0, 0
-        for object_iter in xmltree.findall('object'):
+        # Explore every object
+        for ann_object in doc['annotation']['object']:
 
-            polygon_iter = object_iter.find("polygon")
-
+            # Get bbox of this object
             rectangle = []
-            for pt_iter in polygon_iter.findall("pt"):
-                x = int(pt_iter.find('x').text)
-                y = int(pt_iter.find('y').text)
+            for pts in ann_object['polygon']['pt']:
+                x, y = int(pts['x']), int(pts['y'])
                 coordinate = [x, y]
                 rectangle.append(coordinate)
             rectangle = np.array(rectangle)
 
             x_min, y_min = np.min(rectangle, axis=0)
             x_max, y_max = np.max(rectangle, axis=0)
-            
+
             obj = {
                     "bbox": [x_min, y_min, x_max, y_max],
                     "bbox_mode": BoxMode.XYXY_ABS,
                     "category_id": 0,
+                    # Specify coordinates so that it goes around the boundary.
+                    "segmentation": [[x_min, y_min, x_min, y_max, x_max, y_max, x_max, y_min]],
                 }
-            
+
             objs.append(obj)
-            
+
         record["annotations"] = objs
         dataset_dicts.append(record)
     
